65e27cbd17001433bfbf32cd415f73af466322ef
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-01-21 02:38:24 -0800

    fix: Fixing export

--- src/export.ts
+++ src/export.ts
@@ -4,15 +4,29 @@ import { getComponentAsString } from './index'
 
 const componentName = 'Counter'
 
+function ensureDirectoryExistence(filePath: string): void {
+  const dirname = path.dirname(filePath)
+
+  if (fs.existsSync(dirname)) {
+    return
+  }
+
+  ensureDirectoryExistence(dirname)
+  fs.mkdirSync(dirname)
+}
+
 function exportComponent(destination: string): void {
-  const componentCode = getComponentAsString()
-  const filePath = path.join(destination, `${componentName}/index.tsx`)
+  const componentCode: string = getComponentAsString()
+  const filePath: string = path.join(destination, componentName, 'index.tsx')
+
+  ensureDirectoryExistence(filePath)
 
   fs.writeFileSync(filePath, componentCode, 'utf8')
 
   console.log(`Component code written to ${filePath}`)
 }
 
-const destination = process.argv[2] || 'src/components'
+// The first argument is the destination path in the user's project
+const destination: string = process.argv[2] || 'src/components'
 
 exportComponent(destination)
