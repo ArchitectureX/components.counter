97d5357ddad89b7c5515bca1d9211c54d1db6461
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-01-21 22:30:51 -0800

    fix: Fixing space

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,5 +1,4 @@
-export default () => `
-import React, { FC, useState, CSSProperties } from 'react'
+export default () => `import React, { FC, useState, CSSProperties } from 'react'
 
 type Props = {
   label: string
